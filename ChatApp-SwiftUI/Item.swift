a8efcc383ed9c8d13b34b3a14e990f0e2f89f7ff
karthik.krishna <karthikkrishna104@gmail.com>
2026-09-04 16:41:18 +0530

    Initial Commit

---
+++ ChatApp-SwiftUI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ChatApp-SwiftUI
+//
+//  Created by twixx  on 04/09/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
